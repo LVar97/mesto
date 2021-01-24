b87b8cd902ece6498589d82a244c712da8ff22e7
Varvara Letkova <varvara@MacBook-Pro-Varvara.local>
2021-01-24 13:17:48 +0300

    исправления после ревью-2

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,22 @@
-let Popup = document.querySelector('.popup');
+let windowPopup = document.querySelector('.popup');
 let openButton = document.querySelector('.profile__btn-edit');
 let closeButton = document.querySelector('.popup__close');
-let submitButton = document.querySelector('.btn-submit');
 
 let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__field-name');
 let jobInput = document.querySelector('.popup__field-work');
-let nameValue = nameInput.value;
-let jobValue = jobInput.value;
 let profTitle = document.querySelector('.profile__title');
 let profSubitle = document.querySelector('.profile__subtitle');
 
 
 function showPopup(){
-	Popup.classList.toggle('popup_opened');
+	windowPopup.classList.add('popup_opened');
 	nameInput.value = profTitle.textContent; 
 	jobInput.value = profSubitle.textContent; 
 }
 
 function closePopup(){
-	Popup.classList.toggle('popup_opened');
+	windowPopup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
